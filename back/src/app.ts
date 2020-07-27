53f1a6467561ef9284d11ba39624a24b0e725b8c
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-07-28 01:52:40 +0300

    Add files via upload

---
+++ back/src/app.ts
@@ -0,0 +1,28 @@
+const express=require('express')
+const config=require('config')
+const mongoose = require('mongoose')
+const path=require('path')
+
+const app=express()
+
+app.use(express.json({extended:true}))
+
+const PORT=config.get('port')||5000;
+
+async function start(){
+    try{
+        await mongoose.connect(config.get("mongoURL"),{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            useCreateIndex:true
+
+        })
+        app.listen(PORT,()=>{
+    console.log(`server was started ${PORT}`)
+})
+    }catch(e){
+        console.log('server error',e.message)
+    }
+}
+
+start(); 
