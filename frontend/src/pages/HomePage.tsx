5b7fc03c4d96994fd62be9dc6449bcc9c9cb6beb
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-07-29 17:22:48 +0300

    Add files via upload

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,40 +1,44 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import {useDispatch,useSelector} from 'react-redux'
 import {NavBar} from '../components/navbar/navbar'
 import {Banners} from '../components/content/banners'
 import {Restaurant} from '../components/content/restaurant'
+import {useHttp} from '../hooks/http_hook'
+import {restaurant} from '../interfaces/restaurant'
+import {set_restaurants} from '../redux/actions/restaurants'
+import {RootState} from '../redux/reducers/rootReducer'
 import '../css/content.css';
 import '../css/styles.css';
 
 
 
-let res=[
-  {id:1,name:"burger"},
-  {id:2,name:"burger"},
-  {id:3,name:"burger"},
-  {id:4,name:"burger"},
-  {id:5,name:"burger"},
-  {id:6,name:"burger"},
-  {id:7,name:"burger"},
-  {id:8,name:"burger"},
-  {id:9,name:"burger"},
-  {id:10,name:"burger"},
- 
-]
 
-export const HomePage = () => {
-  
-  
+
+
+export const HomePage:React.FC = () => {
+  const {request}=useHttp()
+  const dispatch=useDispatch()
+  const {fetched_restaurants}=useSelector((state:RootState)=>{
+    return{
+      fetched_restaurants:state.restaurant
+    }
+  })
+
+
+  useEffect(()=>{
+   request('api/restaurant/').then((fetched_restaurant)=>{
+     dispatch(set_restaurants(fetched_restaurant))
+   })  
+  },[request,dispatch])
   return (
     <div className="App">
       <NavBar/>
-      
       <div className="App__content">
         <Banners/>
-        <div className="App__content-main">
-        {res.map(item=>(         
-           <Restaurant />   
-        ))}
-        </div>
+          <div className="App__content-main">
+               
+            <Restaurant  restaurants={[]}/>   
+          </div>
       </div>
       <div className="App__footer"></div>
     </div>
