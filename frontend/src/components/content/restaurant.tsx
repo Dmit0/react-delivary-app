173fc69aab070edce344c7252634b300a90fa46e
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-08-01 21:44:01 +0300

    Add files via upload

--- frontend/src/components/content/restaurant.tsx
+++ frontend/src/components/content/restaurant.tsx
@@ -4,24 +4,23 @@ import {restaurant} from '../../interfaces/restaurant'
 
 
 interface RestaurantProps{
-    restaurants:restaurant[]
+    restaurant:restaurant,
+    onRestaurantClick(restaurant:restaurant):void
 }
 
 
-export const Restaurant:React.FC<RestaurantProps>=({restaurants})=>{
+export const Restaurant:React.FC<RestaurantProps>=({restaurant,onRestaurantClick})=>{
+
    return (
     <>
-    
-    {restaurants.map(item=>(
-        <div key={item._id} className="card-inline">
-            <img src={item.picture}  className="card-img-top" alt="..."/>
+        <div className="card-inline" onClick={()=>onRestaurantClick(restaurant)}>           
+            <img src={restaurant.picture}  className="card-img-top" alt="..."/>
             <div className="App__content-main-card-info">
-                <span className="card-inline-name">{item.name}</span>
-                <span className="card-inline-content">{item.description}</span>
-                <span className="card-inline-time">{item.working_time}</span>
-            </div>
-        </div> 
-    ))}
+                <span className="card-inline-name">{restaurant.name}</span>
+                <span className="card-inline-content">{restaurant.description}</span>
+                <span className="card-inline-time">{restaurant.working_time}</span>
+            </div>           
+        </div>       
     </> 
    )
 }
