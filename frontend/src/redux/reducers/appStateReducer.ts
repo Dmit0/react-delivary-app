fd463b563c3c8d7ac501c958b62e29ccebf6ca5d
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-08-01 21:45:23 +0300

    Add files via upload

--- frontend/src/redux/reducers/appStateReducer.ts
+++ frontend/src/redux/reducers/appStateReducer.ts
@@ -1,10 +1,19 @@
+import {APP_STATES,AppActionTypes,AppState} from '../types/appTypes'
+
 const initialState={
-    
+    loading:false,
+    //bunners:[]
 }
 
-export const appStateReducer=(state=initialState,action:any)=>{
+export const appStateReducer=(state=initialState,action:AppActionTypes):AppState=>{
     
     switch (action.type){
+        case (APP_STATES.SHOW_LOADING):
+            return {...state,loading:true}
+        case (APP_STATES.HIDE_LOADING):
+            return {...state,loading:false}
+        // case (APP_STATES.GET_BUNNERS):
+        //     return {...state,bunners:action.bunners}
         default:return state
     }
 }
--- frontend/src/redux/reducers/restaurantReducer.ts
+++ frontend/src/redux/reducers/restaurantReducer.ts
@@ -1,14 +1,20 @@
-import {RESTAURANTS} from '../actions/types'
+import {RESTAURANTS,restaurantActionTypes,RestaurantState} from '../types/restaurantsTypes'
 
-const initialState={
-    restaurants:[]
+
+const initialState:RestaurantState={
+    restaurants:[],
+    current_restaurant:null
 }
 
-export const restaurantReducer=(state=initialState,action:any)=>{
+export const restaurantReducer=(state=initialState,action:restaurantActionTypes):RestaurantState=>{
     
     switch (action.type){
         case (RESTAURANTS.SET_REASTAURANTS):
-            return {restaurants:action.payload}
+            console.log(action)
+            return {...state}
+        case (RESTAURANTS.SET_CURRENT_RESTAURANT):
+            console.log(action)
+            return {...state}//,current_restaurant:action.}
         default:return state
     }
 }
