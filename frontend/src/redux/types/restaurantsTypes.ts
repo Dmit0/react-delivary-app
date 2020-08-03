e6ff11e2da1af4f8f0d7882a0077bf250548c257
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-08-03 18:04:46 +0300

    Add files via upload

--- frontend/src/redux/types/restaurantsTypes.ts
+++ frontend/src/redux/types/restaurantsTypes.ts
@@ -1,25 +1,41 @@
 import {restaurant} from '../../interfaces/restaurant'
+import {meals} from '../../interfaces/meals'
 
-export const RESTAURANTS ={
-    SET_REASTAURANTS:'SET_REASTAURANTS',
-    SET_CURRENT_RESTAURANT:'SET_CURRENT_RESTAURANT'
-}
+//export const RESTAURANTS ={
+  export const SET_REASTAURANTS='SET_REASTAURANTS';
+  export const SET_CURRENT_RESTAURANT='SET_CURRENT_RESTAURANT';
+  export const SET_INTOSTORE_RESTAURANTS='SET_INTOSTORE_RESTAURANTS';
+  export const SET_MEALS='SET_MEALS'
+//}
 
 interface SET_REASTAURANTS{
-    type:typeof RESTAURANTS.SET_REASTAURANTS
+    type:typeof SET_REASTAURANTS
     restaurants:restaurant[]
 }
 
 interface SET_CURRENT_RESTAURANT{
-    type:typeof RESTAURANTS.SET_CURRENT_RESTAURANT
+    type:typeof SET_CURRENT_RESTAURANT
     restaurant:restaurant | null
 }
+
+interface SET_MEALS{
+    type:typeof SET_MEALS
+    meals:meals[]
+}
+
+
+// interface SET_INTOSTORE_RESTAURANTS{
+//     type:typeof SET_INTOSTORE_RESTAURANTS
+//     payload_restaurants:[]
+// }
+
 
 export interface RestaurantState{
     restaurants:restaurant[],
     current_restaurant:restaurant | null
+    current_meals:meals[]
 }
 
-export type restaurantActionTypes = SET_CURRENT_RESTAURANT | SET_REASTAURANTS
+export type restaurantActionTypes = SET_CURRENT_RESTAURANT | SET_REASTAURANTS | SET_MEALS
 
 
