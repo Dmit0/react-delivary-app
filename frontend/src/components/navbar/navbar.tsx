657b55102545c127269d0a58a04ce17d41907e84
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-08-07 20:06:30 +0300

    Add files via upload

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,33 +1,39 @@
 import React from 'react';
 import '../../css/header.css';
+import {Link} from 'react-router-dom'
 
-export const NavBar=()=>{
+interface NavBarProps{
+  cart_length?:number
+}
+
+export const NavBar:React.FC<NavBarProps>=({cart_length})=>{
     return(
         <div className="App__header ">
         <div className="App_header__main">
             <nav className="navbar navbar-light">
               <div className="container-fluid App_header__main-container ">
-                <span className="navbar-brand  App_header__main-Header">
-                    <img src="./assets/leaf.svg" width="30" height="30" alt="" loading="lazy"/>
-                  Delivary
-                </span>
-                <button type="button" className="btn btn-outline-info App_header__main-button">Log In</button>
+                <Link to={`/HomePage`}> 
+                  <span className="navbar-brand  App_header__main-Header">
+                      <img src="./assets/leaf.svg" width="30" height="30" alt="" loading="lazy"/>
+                      Delivary
+                  </span>
+                </Link>
+                <input className="form-control mr-2 App_header__secondary-input" type="search" placeholder="Search" aria-label="Search"/>
+                <div className="button-controller">
+                  <button type="button" className="btn btn-outline-info App_header__main-button">LogIn</button>
+    <Link to={'/cart'}><button type="button" className="btn btn-warning App_header__secondary-button">Cart<span className="App_header__secondary-cart">{cart_length}</span></button></Link>
+                </div>
               </div>
               </nav>
             </div>
-            <form className="">
-                    <input className="form-control mr-2 App_header__secondary-input" type="search" placeholder="Search" aria-label="Search"/>
-                  </form>
         <div className="App_header__secondary-wrapper">
           <div className="App_header__secondary">
               <nav className="navbar">
                 <div className="container-fluid  App_header__secondary-container">
                   <div className="App_header__secondary-action">
-                    <span className="navbar-brand">All</span>
-                    <span className="navbar-brand">Loved</span>
+                    <span className="navbar-brand-menu">All</span>
+                    <span className="navbar-brand-menu">Loved</span>
                   </div>
-                  
-                  <button type="button" className="btn btn-warning App_header__secondary-button">Cart<span className="App_header__secondary-cart">0</span></button>
                 </div>
               </nav>   
           </div>
