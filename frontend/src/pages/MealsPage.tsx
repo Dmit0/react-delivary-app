761c910e131f69b101c68cc6d3ada3597c78fffb
Dmitrii Shehunov <53045487+Dmit0@users.noreply.github.com>
2020-08-14 15:24:20 +0300

    Add files via upload

--- frontend/src/pages/MealsPage.tsx
+++ frontend/src/pages/MealsPage.tsx
@@ -1,7 +1,9 @@
 import React,{useEffect} from 'react';
 import {useSelector,useDispatch} from 'react-redux'
+import {useParams} from 'react-router-dom'
 import '../css/meals-content.css'
 import{set_meal_to_cart,set_meal_from_localestorage_to_cart} from '../redux/actions/cartActions'
+import {get_meals_by_restaurant_id} from '../redux/actions/restaurants'
 import {RootState} from '../redux/reducers/rootReducer'
 import {NavBar} from '../components/navbar/navbar'
 import {Meal} from '../components/content/meal'
@@ -10,7 +12,8 @@ import {meals as Mealtype} from '../interfaces/meals'
 
 
 export const MealsPage:React.FC = () => {
-
+    
+    let {id}=useParams();
     const dispatch=useDispatch()
     const {meals,cart}=useSelector((state:RootState)=>{
         return {
@@ -24,22 +27,26 @@ export const MealsPage:React.FC = () => {
     }
 
     useEffect(()=>{
-        const cart_items=JSON.parse(localStorage.getItem('cart') || '[]') as Mealtype[]
-        dispatch(set_meal_from_localestorage_to_cart(cart_items))
+
+            const cart_items=JSON.parse(localStorage.getItem('cart') || '[]') as Mealtype[]
+            dispatch(set_meal_from_localestorage_to_cart(cart_items))            
         // eslint-disable-next-line
        },[])
 
-    useEffect(()=>{
-        if(cart.length>0){
-          localStorage.setItem('cart',JSON.stringify(cart))
-        }
+    useEffect(()=>{   
+          localStorage.setItem('cart',JSON.stringify(cart)) 
       },[cart])
+
+      useEffect(()=>{
+        dispatch(get_meals_by_restaurant_id(id))
+         // eslint-disable-next-line
+      },[])
    
 
     return (
         <>
          <div className="App">
-            <NavBar cart_length={cart.length}/>
+            <NavBar cart_length={cart}/>
          <div className="App__meals-container">
             <div className="App__content-main">
                 {meals.map(meal=>(
