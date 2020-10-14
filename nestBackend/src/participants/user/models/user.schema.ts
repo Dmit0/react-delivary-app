22fb1278300c1d9854c199ca9411fb4f643f22ce
roman-asanovich <roman.asanovich@intexsoft.by>
2020-10-14 16:22:27 +0300

    adding user entity

---
+++ nestBackend/src/participants/user/models/user.schema.ts
@@ -0,0 +1,48 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export enum UserStatuses {
+  BLOCKED = 'blocked',
+  ACTIVE = 'active',
+  DEACTIVATED = 'deactivated',
+}
+
+@Schema()
+export class User extends Document {
+  @Prop({ require: true })
+  name: string;
+
+  @Prop({ require: true, unique: true })
+  email: string;
+
+  @Prop({ require: true })
+  password: string;
+
+  @Prop({ require: true })
+  telephone: string;
+
+  @Prop()
+  createdAt: string;
+
+  @Prop({
+    type: 'enum',
+    enum: UserStatuses,
+    default: UserStatuses.ACTIVE,
+  })
+  status: UserStatuses;
+
+  @Prop()
+  role: { type: Types.ObjectId, ref: 'Role' };
+
+  @Prop()
+  ownership: [{ type: Types.ObjectId, ref: 'Restaurant' }];
+
+  @Prop()
+  lovedReastaurant: [{ type: Types.ObjectId, ref: 'Restaurant' }];
+
+  @Prop()
+  cart: [{ type: Types.ObjectId, ref: 'Meal' }];
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
---
+++ nestBackend/src/participants/user/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './models/user.schema';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel(User.name) private restaurantModel: Model<User>,
+  ) {
+  }
+
+  getUser() {
+    return
+  }
+  createUser(){
+    return
+  }
+}
